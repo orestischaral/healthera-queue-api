630ce89ba24e3d2671b3fe33698be5752bb1fed0
Orestis Charalampakos <orestischaral@gmail.com>
2025-11-22 19:39:36 +0200

    Add command , query handlers and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
 import configuration from '@infrastructure/config/configuration';
+import { QueueModule } from '@infrastructure/queue/QueueModule';
+import { QueueController } from '@infrastructure/http/controllers/QueueController';
+import { MessageController } from '@infrastructure/http/controllers/MessageController';
+import { HealthController } from '@infrastructure/http/controllers/HealthController';
+import { PublishMessageHandler } from '@application/queue/commands/PublishMessageHandler';
+import { CreateQueueHandler } from '@application/queue/commands/CreateQueueHandler';
+import { DeleteQueueHandler } from '@application/queue/commands/DeleteQueueHandler';
+import { AcknowledgeMessageHandler } from '@application/queue/commands/AcknowledgeMessageHandler';
+import { ReceiveMessagesHandler } from '@application/queue/queries/ReceiveMessagesHandler';
+
+const CommandHandlers = [
+  PublishMessageHandler,
+  CreateQueueHandler,
+  DeleteQueueHandler,
+  AcknowledgeMessageHandler,
+];
+
+const QueryHandlers = [ReceiveMessagesHandler];
 
 @Module({
   imports: [
@@ -8,8 +27,10 @@ import configuration from '@infrastructure/config/configuration';
       isGlobal: true,
       load: [configuration],
     }),
+    CqrsModule,
+    QueueModule.forRootAsync(),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [QueueController, MessageController, HealthController],
+  providers: [...CommandHandlers, ...QueryHandlers],
 })
 export class AppModule {}
