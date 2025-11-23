94e42be68a31623eefdd63da6985b81ea1958d07
Orestis Charalampakos <orestischaral@gmail.com>
2025-11-23 16:30:23 +0200

    Use NotificationService in publish message handler

--- src/application/queue/commands/PublishMessageHandler.ts
+++ src/application/queue/commands/PublishMessageHandler.ts
@@ -2,12 +2,16 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { QueueMessage } from '@domain/queue/entities/QueueMessage';
 import { IQueueProvider } from '@domain/queue/interfaces/IQueueProvider';
 import { PublishMessageCommand } from './PublishMessageCommand';
+import { INotificationService } from '@app/domain/notification/interfaces/INotificationService';
 
 @CommandHandler(PublishMessageCommand)
 export class PublishMessageHandler
   implements ICommandHandler<PublishMessageCommand>
 {
-  constructor(private readonly queueProvider: IQueueProvider) {}
+  constructor(
+    private readonly queueProvider: IQueueProvider,
+    private readonly notificationService: INotificationService,
+  ) {}
 
   async execute(command: PublishMessageCommand): Promise<string> {
     const message = QueueMessage.create(
@@ -15,7 +19,12 @@ export class PublishMessageHandler
       command.payload,
       command.attributes,
     );
+    const messageId = await this.queueProvider.publish(message);
+    await this.notificationService.notifyMessagePublished(
+      command.queueName,
+      messageId,
+    );
 
-    return this.queueProvider.publish(message);
+    return messageId;
   }
 }
